2a7c92b87fc107a908b51a1f49c130caf641b7e3
Taylor Otwell <taylor@laravel.com>
2025-08-07 13:54:05 -0500

    remove unnecessary files

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,25 +1,7 @@
-import { LucideIcon } from 'lucide-react';
 import type { Config } from 'ziggy-js';
 
 export interface Auth {
     user: User;
-}
-
-export interface BreadcrumbItem {
-    title: string;
-    href: string;
-}
-
-export interface NavGroup {
-    title: string;
-    items: NavItem[];
-}
-
-export interface NavItem {
-    title: string;
-    href: string;
-    icon?: LucideIcon | null;
-    isActive?: boolean;
 }
 
 export interface SharedData {
@@ -27,7 +9,6 @@ export interface SharedData {
     quote: { message: string; author: string };
     auth: Auth;
     ziggy: Config & { location: string };
-    sidebarOpen: boolean;
     [key: string]: unknown;
 }
 
